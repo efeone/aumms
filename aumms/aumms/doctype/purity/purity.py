8ad93555427f026ef420df0d37aeab36bd9ed7fd
Aswani m <m.aswanibabu@efeone.com>
2023-01-07 09:28:34 +0530

    fix: Validate purity percentage

--- aumms/aumms/doctype/purity/purity.py
+++ aumms/aumms/doctype/purity/purity.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
+
 
 class Purity(Document):
-	pass
+	def validate(self):
+		self.validate_purity_percentage()
+
+	def validate_purity_percentage(self):
+		#Method to validate Purity Percentage#
+		if self.purity_percentage < 0 or self.purity_percentage > 100:
+			frappe.throw(
+				title = _('ALERT !!'),
+				msg = _(' Purity Percentage must range from 0 to 100')
+				)
