8a8eecee253558df5fa5931c0e2482c3ff691751
Anjana-sajeevan <anjanasajeevs@gmail.com>
2023-01-09 13:53:29 +0530

    feat: Get item details to Sales invoice item on selection of item

--- aumms/aumms/doc_events/sales_invoice.py
+++ aumms/aumms/doc_events/sales_invoice.py
@@ -1,17 +1,15 @@
 import frappe
+from aumms.aumms.utils import *
 
-@frappe.whitelist()#function for qty, making_charge_percentage, making_charge & board_rate fetching
-def fetch_qty(item,type):
-    qty = ''
-    charge_percentage = ''
-    charge = ''
-    rate = ''
-    if item:
-        item_doc = frappe.get_doc('Item', item)
-        qty = item_doc.weight_per_unit
-        charge_percentage = item_doc.making_charge_percentage
-        charge = item_doc.making_charge
-    if type:
-        b_rate = frappe.get_last_doc('Board Rate', {'item_type':type})
-        rate = b_rate.board_rate
-    return (qty, charge_percentage, charge, rate)
+@frappe.whitelist()
+def get_item_details(item_code, item_type, date, time, purity):
+    ''' Method for fetching qty, making_charge_percentage, making_charge & board_rate '''
+    item_details = { 'qty':0, 'making_charge_percentage':0, 'making_charge':0,  'board_rate':0  }
+    if item_code:
+        item_doc = frappe.get_doc('Item', item_code)
+        item_details['qty'] = item_doc.weight_per_unit
+        item_details['making_charge_percentage'] = item_doc.making_charge_percentage
+        item_details['making_charge'] = item_doc.making_charge
+        board_rate = get_board_rate(date, time, item_type, item_doc.stock_uom, purity)
+        item_details['board_rate'] = board_rate
+    return item_details
