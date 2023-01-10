62b48746339166b4414dc6bbdab1e73ea7d8cbeb
shamilnk <shamilnk91@gmail.com>
2023-01-10 12:24:48 +0530

    fix: validation for purity percentage on change of field

--- aumms/aumms/doctype/purity/purity.js
+++ aumms/aumms/doctype/purity/purity.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Purity', {
-	// refresh: function(frm) {
 
-	// }
+	purity_percentage (frm) {
+
+		if (frm.doc.purity_percentage) {
+
+			// check purity percentage is not between 0 and 100
+			if(0 > frm.doc.purity_percentage || frm.doc.purity_percentage  > 100) {
+
+				// message for user about purity percentage range
+				frappe.throw({
+					title: __('ALERT !!'),
+					message: __('Purity Percentage must range from 0 to 100')
+				});
+
+			}
+		}
+
+	}
+
 });
