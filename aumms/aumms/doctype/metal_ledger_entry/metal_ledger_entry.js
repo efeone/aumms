437d46dfd5eb6430b450739ae741bffc54682547
Aswani m <m.aswanibabu@efeone.com>
2023-01-13 16:54:30 +0530

    chore: Set Metal Ledger Entry read only for all users and also disable save button

--- aumms/aumms/doctype/metal_ledger_entry/metal_ledger_entry.js
+++ aumms/aumms/doctype/metal_ledger_entry/metal_ledger_entry.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Metal Ledger Entry', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+    //Disable save button from metal ledger entry
+    frm.disable_save();
+}
 });
