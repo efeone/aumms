9b11c6e313c33ffc3260bd6cb7e5a669bd6c2dda
Anjana-sajeevan <anjanasajeevs@gmail.com>
2023-01-13 15:20:38 +0530

    feat: Created Metal Ledger report

---
+++ aumms/aumms/report/metal_ledger/metal_ledger.js
@@ -0,0 +1,102 @@
+// Copyright (c) 2023, efeone and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports['Metal Ledger'] = {
+	'filters': [
+		{
+			'fieldname': 'company',
+			'label': __('Company'),
+			'fieldtype': 'Link',
+			'options': 'Company',
+			'default': frappe.defaults.get_user_default('Company'),
+			'reqd': 1
+		},
+		{
+			'fieldname': 'from_date',
+			'label': __('From Date'),
+			'fieldtype': 'Date',
+			'default': frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			'reqd': 1
+		},
+		{
+			'fieldname': 'to_date',
+			'label': __('To Date'),
+			'fieldtype': 'Date',
+			'default': frappe.datetime.get_today(),
+			'reqd': 1
+		},
+		{
+			'fieldname': 'item_code',
+			'label': __('Item'),
+			'fieldtype': 'Link',
+			'options': 'Item',
+			'get_query': function() {
+				return {
+					query: 'erpnext.controllers.queries.item_query'
+				}
+			}
+		},
+		{
+			'fieldname': 'item_type',
+			'label': __('Item Type'),
+			'fieldtype': 'Link',
+			'options': 'Item Type'
+		},
+		{
+			'fieldname': 'batch_no',
+			'label': __('Batch No'),
+			'fieldtype': 'Link',
+			'options': 'Batch'
+		},
+		{
+			'fieldname': 'voucher_type',
+			'label': __('Voucher Type'),
+			'fieldtype': 'Link',
+	    'options': 'DocType',
+			'get_query': function() {
+				return {
+					'filters': {
+						"name": ['in',['Purchase Receipt', 'Sales Invoice']]
+					}
+				}
+			}
+		},
+		{
+			'fieldname': 'voucher_no',
+			'label': __('Voucher No'),
+			'fieldtype': 'Dynamic Link',
+	    'options': 'voucher_type'
+		},
+		{
+			'fieldname': 'uom',
+			'label': __('UOM'),
+			'fieldtype': 'Link',
+			'options': 'UOM'
+		},
+		{
+			'fieldname': 'party_type',
+			'label': __('Party Type'),
+			'fieldtype': 'Link',
+			'options': 'DocType',
+			'get_query': function() {
+				return {
+					'filters': {
+						"name": ['in',['Supplier', 'Customer']]
+					}
+				}
+			}
+		},
+		{
+			'fieldname': 'party',
+			'label': __('Party'),
+			'fieldtype': 'Dynamic Link',
+			'options': 'party_type'
+		},
+		{
+			'fieldname': 'is_cancelled',
+			'label': __('Is cancelled'),
+			'fieldtype': 'Check'
+		}
+	]
+};
---
+++ aumms/aumms/report/metal_ledger/metal_ledger.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2023, efeone and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import *
+from frappe import _
+
+def execute(filters=None):
+	columns, data = get_columns(), get_data(filters)
+	return columns, data
+
+def get_columns():
+	''' Method to get columns in report '''
+	columns = [
+	    {'label': _('Item Code'), 'fieldtype': 'Link', 'options': 'Item', 'width': 200},
+		{'label': _('Item Name'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Posting Date'), 'fieldtype': 'Date', 'width': 200},
+		{'label': _('Posting Time'), 'fieldtype': 'Time', 'width': 200},
+		{'label': _('Voucher Type'), 'fieldtype': 'Link', 'options': 'DocType', 'fieldname': 'voucher_type', 'width': 200},
+		{'label': _('Voucher No'), 'fieldtype': 'Dynamic Link', 'options': 'voucher_type', 'width': 200},
+		{'label': _('Party Type'), 'fieldtype': 'Link', 'options': 'DocType', 'width': 200},
+		{'label': _('Party'), 'fieldtype': 'Link', 'options': 'DocType', 'width': 200},
+		{'label': _('Stock UOM'), 'fieldtype': 'Link', 'options': 'UOM', 'width': 200},
+		{'label': _('Item Type'), 'fieldtype': 'Link', 'options': 'Item Type', 'width': 200},
+		{'label': _('Purity'), 'fieldtype': 'Link', 'options': 'Purity', 'width': 200},
+		{'label': _('Purity Percentage'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Quantity'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Board Rate'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Incoming Rate'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Outgoing Rate'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Amount'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Company'), 'fieldtype': 'Link', 'options': 'Company', 'width': 200},
+		{'label': _('Batch No'), 'fieldtype': 'Data', 'width': 150},
+		{'label': _('Is Cancelled'), 'fieldtype': 'Check', 'width': 150}
+		]
+	return columns
+
+def get_data(filters):
+	''' Method to get data '''
+	get_filters(filters)
+	data = []
+	metal_ledger_entry_list = frappe.get_all('Metal Ledger Entry', order_by='creation desc', filters=get_filters(filters))
+	for metal_ledger_entry in metal_ledger_entry_list:
+		doc = frappe.get_doc('Metal Ledger Entry', metal_ledger_entry.name)
+		row = [
+			doc.item_code,
+			doc.item_name,
+			doc.posting_date,
+			doc.posting_time,
+			doc.voucher_type,
+			doc.voucher_no,
+			doc.party_type,
+			doc.party,
+			doc.stock_uom,
+			doc.item_type,
+			doc.purity,
+			doc.purity_percentage,
+			doc.qty,
+			doc.board_rate,
+			doc.incoming_rate,
+			doc.outgoing_rate,
+			doc.amount,
+			doc.company,
+			doc.batch_no,
+			doc.is_cancelled
+		]
+		data.append(row)
+	return data
+
+def get_filters(filters):
+	''' Method to get filters '''
+	conditions = {}
+	if filters.company:
+		conditions['company'] = filters.company
+	if filters.from_date and filters.to_date:
+		conditions['posting_date'] = [ 'between' , [ getdate(filters.from_date), getdate(filters.to_date) ] ]
+	if filters.item_code:
+		conditions['item_code'] = filters.item_code
+	if filters.is_cancelled:
+		conditions['is_cancelled'] = filters.is_cancelled
+	if filters.purity:
+		conditions['purity'] = filters.purity
+	if filters.uom:
+		conditions['stock_uom'] = filters.uom
+	if filters.item_type:
+		conditions['item_type'] = filters.item_type
+	if filters.party_type:
+		conditions['party_type'] = filters.party_type
+	if filters.party:
+		conditions['party'] = filters.party
+	if filters.voucher_type:
+		conditions['voucher_type'] = filters.voucher_type
+	if filters.voucher_no:
+		conditions['voucher_no'] = filters.voucher_no
+	if filters.item_type:
+		conditions['item_type'] = filters.item_type
+	if conditions:
+		return conditions
+	else:
+		return []
