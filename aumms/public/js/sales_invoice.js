acf792f9cb3252e563d3967d56d384d866edd1cb
akshayr11 <akshay.r.ravi@gmail.com>
2023-01-13 15:02:06 +0530

    feat:set calculations in sales invoice

--- aumms/public/js/sales_invoice.js
+++ aumms/public/js/sales_invoice.js
@@ -16,12 +16,78 @@ frappe.ui.form.on('Sales Invoice Item', {
           if (r.message){
             d.qty = r.message['qty']
             d.making_charge_percentage = r.message['making_charge_percentage']
-            d.making_charge = r.message['making_charge']
+            d.is_fixed_making_charge = r.message['making_charge']
             d.board_rate = r.message['board_rate']
             d.making_charge_based_on = r.message ['making_charge_based_on']
+            d.amount_with_out_making_charge = r.message['qty'] * r.message['board_rate']
+            setTimeout(() => {
+             frappe.model.set_value(d.doctype, d.name, 'rate', ((r.message['qty'] * r.message['board_rate']) + r.message['making_charge'])/r.message['qty'])// set time out of 500 ms for  rate
+           }, 500);
+            if (r.message['making_charge']){
+              d.making_charge = r.message['making_charge']
+            }
+            else {
+                d.making_charge = (d.amount_with_out_making_charge)*(d.making_charge_percentage * 0.01)//set making_charge if it's percentage
+              }
           }
         }
       })
+    }
+  },
+  qty: function(frm, cdt, cdn){
+
+    let d = locals[cdt][cdn];
+    if (d.qty){
+      frappe.model.set_value(d.doctype, d.name, 'amount_with_out_making_charge', d.qty * d.board_rate);//set amount_with_out_making_charge while changing qty
+
+    }
+  },
+  making_charge_percentage: function(frm, cdt, cdn){
+
+    let d = locals[cdt][cdn];
+    if (d.making_charge_percentage){
+      var making_charge = d.amount_with_out_making_charge * d.making_charge_percentage * 0.01
+      frappe.model.set_value(d.doctype, d.name, 'making_charge', making_charge);//set making_charge while changing of making_charge_percentage
+    }
+    frm.refresh_field('items')
+  },
+  amount_with_out_making_charge: function(frm, cdt, cdn){
+
+    let d = locals[cdt][cdn];
+    if (d.amount_with_out_making_charge){
+      var making_charge = d.amount_with_out_making_charge * d.making_charge_percentage * 0.01
+      frappe.model.set_value(d.doctype, d.name, 'making_charge', making_charge);//set making_charge while changing of amount_with_out_making_charge
+    }
+    frm.refresh_field('items')
+  },
+  making_charge:function (frm, cdt, cdn) {
+
+    let d = locals[cdt][cdn];
+    if(d.making_charge) {
+      let rate = (d.amount_with_out_making_charge + d.making_charge)/d.qty
+      if (rate)
+      {
+        frappe.model.set_value(d.doctype, d.name, 'rate', rate);//set rate by the change of making_charge
+      }
+    }
+  },
+  amount_with_out_making_charge:function (frm, cdt, cdn) {
+
+    let d = locals[cdt][cdn];
+    if(d.amount_with_out_making_charge) {
+      let rate = (d.amount_with_out_making_charge + d.making_charge)/d.qty
+      if (rate)
+      {
+        frappe.model.set_value(d.doctype, d.name, 'rate', rate);//set rate by the change of amount_with_out_making_charge
+      }
+    }
+  },
+  conversion_factor:function(frm, cdt,cdn){
+
+    let d = locals[cdt][cdn];
+    if(d.conversion_factor){
+      var rate = d.board_rate * d.conversion_factor
+      frappe.model.set_value(d.doctype, d.name, 'rate', rate);//set rate by the change of conversion_factor
     }
   }
 })
