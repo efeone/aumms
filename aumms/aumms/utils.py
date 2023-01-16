1561e34165979028619e581fa8903019d93da621
rijilprakashan <rijilprakashan@gmail.com>
2023-01-16 15:15:38 +0530

    feat: Fetch Board Rate to rate field of Purchase Receipt items

--- aumms/aumms/utils.py
+++ aumms/aumms/utils.py
@@ -3,11 +3,11 @@ from frappe.utils import *
 from frappe import _
 
 @frappe.whitelist()
-def get_board_rate(date, time, item_type, uom, purity=None):
+def get_board_rate(date, time, item_type, purity=None):
     ''' Method to get Board Rate '''
     board_rate = 0
     if purity:
-        filters = { 'docstatus': '1', 'item_type': item_type, 'date':getdate(date), 'time': [ '<=', time ], 'purity': purity }
+        filters = { 'docstatus': '1', 'item_type': item_type, 'date':[ '<=', getdate(date) ], 'time': [ '<=', time ], 'purity': purity }
     else:
         filters = { 'docstatus': '1', 'item_type': item_type, 'date':getdate(date), 'time': [ '<=', time ] }
     if frappe.db.get_all('Board Rate', filters=filters):
--- aumms/public/js/purchase_receipt.js
+++ aumms/public/js/purchase_receipt.js
@@ -1,30 +1,82 @@
 frappe.ui.form.on('Purchase Receipt', {
-  is_metal_transaction (frm) {
+  is_metal_transaction(frm) {
     //Check is metal transaction
-    if(frm.doc.is_metal_transaction){
+    if (frm.doc.is_metal_transaction) {
       change_is_metal_transaction(frm, 1)
     }
-    else{
+    else {
       change_is_metal_transaction(frm, 0)
     }
     frm.refresh_field('items')
   }
 });
 
-frappe.ui.form.on('Purchase Receipt Item',{
-  items_add(frm, cdt, cdn){
+//declare board_rate
+let board_rate;
+
+frappe.ui.form.on('Purchase Receipt Item', {
+  items_add(frm, cdt, cdn) {
     let child = locals[cdt][cdn]
     //Check is metal transaction
-    if(frm.doc.is_metal_transaction){
+    if (frm.doc.is_metal_transaction) {
       //set value to the field is is_metal_transaction as 1
       frappe.model.set_value(child.doctype, child.name, 'is_metal_transaction', 1)
     }
+  },
+  item_code(frm, cdt, cdn) {
+    let child = locals[cdt][cdn]
+
+    if (child.item_code) {
+      board_rate = 0
+      //to make delay in board_rate entry
+      setTimeout(function () {
+        //call to get board_rate
+        frappe.call({
+          method: 'aumms.aumms.utils.get_board_rate',
+          args: {
+            item_code: child.item_code,
+            item_type: child.item_type,
+            date: frm.doc.posting_date,
+            time: frm.doc.posting_time,
+            purity: child.purity
+          },
+          callback: function (r) {
+            if (r.message) {
+
+                board_rate = r.message
+                if (child.conversion_factor) {
+                  //change rate in accordance with conversion_factor
+                  board_rate = board_rate * child.conversion_factor
+                }
+                //set value to rate field
+                frappe.model.set_value(child.doctype, child.name, 'rate', board_rate)
+
+            }
+          }
+        })
+      }, 500);
+    }
+
+  },
+  conversion_factor (frm, cdt, cdn) {
+    let child = locals[cdt][cdn]
+
+    if (!board_rate) {
+      // change board rate as rate
+      board_rate = child.rate
+    }
+    if (child.conversion_factor) {
+
+      let rate = board_rate * child.conversion_factor
+      //set value to rate field
+      frappe.model.set_value(child.doctype, child.name, 'rate', rate)
+
+    }
   }
+})
 
-});
 
-
-let change_is_metal_transaction = function (frm, value){
+let change_is_metal_transaction = function (frm, value) {
   /*
     function to iterate item table and set value to the field is_metal_transaction
     args:
