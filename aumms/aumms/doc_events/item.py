c1ca863e56961063c31de49d2bfae1bb6661a95f
akshayr11 <akshay.r.ravi@gmail.com>
2023-01-19 16:34:21 +0530

    fix:in item_group percentage and currency is while the change of making_charge_based_on and in item set making_charge_percentage and making_charge while the change of making_charge_based_on

--- aumms/aumms/doc_events/item.py
+++ aumms/aumms/doc_events/item.py
@@ -68,7 +68,7 @@ def making_charge_to_item(item_group, charge_based_on, type):
     return percentage
 
 @frappe.whitelist()
-def making_charge_to_item_1(item_group, charge_based_on, type):
+def fetch_making_charge_from_item_group_to_item(item_group, charge_based_on, type):
     '''set making_charge_percentage and currency while the change of making_charge_based_on'''
     item_group_details = {'percentage': 0, 'currency':0}
     if charge_based_on == 'Percentage':
