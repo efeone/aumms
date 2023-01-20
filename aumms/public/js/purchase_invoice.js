7a220ab08ad31c1d53709d8bdd737c5a3adac1b8
Anjana-sajeevan <anjanasajeevs@gmail.com>
2023-01-20 10:26:26 +0530

    fix: Renamed Is Metal Transaction into Keep Metal Ledger

--- aumms/public/js/purchase_invoice.js
+++ aumms/public/js/purchase_invoice.js
@@ -1,11 +1,11 @@
 frappe.ui.form.on('Purchase Invoice', {
-  is_metal_transaction (frm) {
-    //Check is metal transaction
-    if(frm.doc.is_metal_transaction){
-      change_is_metal_transaction(frm, 1)
+  keep_metal_ledger (frm) {
+    //Checking the keep metal ledger
+    if(frm.doc.keep_metal_ledger){
+      change_keep_metal_ledger(frm, 1)
     }
     else{
-      change_is_metal_transaction(frm, 0)
+      change_keep_metal_ledger(frm, 0)
     }
     frm.refresh_field('items')
   }
@@ -14,24 +14,24 @@ frappe.ui.form.on('Purchase Invoice', {
 frappe.ui.form.on('Purchase Invoice Item',{
   items_add(frm, cdt, cdn){
     let child = locals[cdt][cdn]
-    //Check is metal transaction
-    if(frm.doc.is_metal_transaction){
-      //set value to the field is is_metal_transaction as 1
-      frappe.model.set_value(child.doctype, child.name, 'is_metal_transaction', 1)
+    //Checking the keep metal ledger
+    if(frm.doc.keep_metal_ledger){
+      //set value to the field is keep_metal_ledger as 1
+      frappe.model.set_value(child.doctype, child.name, 'keep_metal_ledger', 1)
     }
   }
 
 });
 
 
-let change_is_metal_transaction = function (frm, value){
+let change_keep_metal_ledger = function (frm, value){
   /*
-    function to iterate item table and set value to the field is_metal_transaction
+    function to iterate item table and set value to the field keep_metal_ledger
     args:
-      frm: current form of purchase Receipt
+      frm: current form of purchase Invoice
       value: Boolean
   */
   frm.doc.items.forEach((item) => {
-    item.is_metal_transaction = value
+    item.keep_metal_ledger = value
   });
 }
