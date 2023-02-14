64d4f99711103b1ac2fc4e8b2b334f467749b23f
shamilnk <shamilnk91@gmail.com>
2023-02-14 11:32:07 +0530

    fix: filter for item type with is purity in board rate

--- aumms/aumms/doctype/board_rate/board_rate.js
+++ aumms/aumms/doctype/board_rate/board_rate.js
@@ -3,18 +3,28 @@
 
 frappe.ui.form.on('Board Rate', {
 	onload(frm) {
-		frm.set_query('uom', function() {
-			return {
-				filters: {
-					is_purity_uom : 1
-				}
-			};
-		});
+		set_filter('uom', {is_purity_uom: 1})
+		set_filter('item_type', {is_purity_item: 1})
 	},
-	validate(frm){
+	validate(frm) {
 		set_title_name(frm)
 	}
 });
-function set_title_name(frm){//set title for board_rate
+function set_title_name(frm) {//set title for board_rate
 	frm.set_value('title', frm.doc.item_type + '-' + frm.doc.purity + '-' + frm.doc.board_rate);
 }
+
+let set_filter = function (field, filters) {
+	/*
+		function to set filter for a specific field
+		args:
+			field: field name
+			filters: set of filters, (eg: {key:value})
+		output: filter applied list for field
+	*/
+	cur_frm.set_query(field, () => {
+	  return {
+		filters: filters
+	  }
+	})
+  }
