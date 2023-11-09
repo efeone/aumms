f9ecff4a4054190c1a03d88cd0dd582ca2b7cedc
Muhammed Sinan K T <muhammed786sinu000@gmail.com>
2023-11-09 12:38:46 +0530

    feat: Customisations in Smith DocType

--- aumms/aumms/doctype/smith/smith.js
+++ aumms/aumms/doctype/smith/smith.js
@@ -2,7 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Smith', {
-	// refresh: function(frm) {
-
-	// }
+	smith_type: function(frm) {
+		reset_smith_fields(frm)
+	},
+	employee: function(frm) {
+		set_smith_full_name(frm)
+	},
+	supplier:  function(frm) {
+		set_smith_full_name(frm)
+	}
 });
+
+function set_smith_full_name(frm) {
+	// Fetching a smith's name from employee or supplier depending on the type
+	if (frm.doc.smith_type == 'Internal') {
+		frappe.db.get_value('Employee', frm.doc.employee, 'employee_name')
+			.then(r => {
+				let smith_full_name = r.message.employee_name
+				frm.set_value('smith_name', smith_full_name)
+			})
+	}
+	else if (frm.doc.smith_type == 'External') {
+		frappe.db.get_value('Supplier', frm.doc.supplier, 'supplier_name')
+			.then(r => {
+				let smith_full_name = r.message.supplier_name
+				frm.set_value('smith_name', smith_full_name)
+				frm.refresh_fields()
+			})
+	}
+}
+
+function reset_smith_fields(frm) {
+	// Resetting the fields when smith type is changed
+	frm.set_value('smith_name', '')
+	frm.set_value('employee', '')
+	frm.set_value('supplier', '')
+	frm.refresh_fields()
+}
