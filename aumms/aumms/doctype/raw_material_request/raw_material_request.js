57036ad29ecfe748b65ba3504b5b83ce6bfeeb41
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-03-13 13:30:49 +0530

    Doctype Manufacturing request created

--- aumms/aumms/doctype/raw_material_request/raw_material_request.js
+++ aumms/aumms/doctype/raw_material_request/raw_material_request.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Raw Material Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Raw Material Request", {
+  setup : function(frm){
+    frm.set_query('uom', 'raw_material_details', ()=>{
+      return{
+        filters :{
+          "is_purity_uom" : 1
+        }
+      }
+    })
+  }
+});
