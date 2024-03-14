f446f6a8fa64a0bfabf9d45c8a98c68b8988eb9c
Keerthana-ck <keerthana98323@gmail.com>
2024-03-14 13:09:05 +0530

    feat : Created Jewellery Order Throught Customer Jewellery Order

--- aumms/aumms/doctype/customer_jewellery_order/customer_jewellery_order.js
+++ aumms/aumms/doctype/customer_jewellery_order/customer_jewellery_order.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Customer Jewellery Order", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Customer Jewellery Order", {
+	refresh(frm) {
+    if (!frm.is_new()){
+      frm.add_custom_button('Jewellery Order', () => {
+        frappe.call('aumms.aumms.doctype.customer_jewellery_order.customer_jewellery_order.create_jewellery_order', {
+          customer_jewellery_order : frm.doc.name
+        }).then(r =>{
+            frm.reload_doc();
+        });
+      },'Create');
+    }
+	},
+});
