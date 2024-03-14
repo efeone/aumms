7b457d2ebc496006bb0de0fbfb77bba7c4c4a2d9
Geethu K <geethuk626@gmail.com>
2024-03-14 13:25:56 +0530

    feat: The Customer Expected Total Weight must be sum of Expected Weight Per Quantity in the child table

--- aumms/aumms/doctype/customer_jewellery_order/customer_jewellery_order.py
+++ aumms/aumms/doctype/customer_jewellery_order/customer_jewellery_order.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class CustomerJewelleryOrder(Document):
-	pass
+
+    def validate(self):
+        if self.total_expected_weight_per_quantity != self.customer_expected_total_weight:
+            frappe.throw(_('The Sum of Expected Weights Per Quantity must be Equal to Customer Expected Weight'))
