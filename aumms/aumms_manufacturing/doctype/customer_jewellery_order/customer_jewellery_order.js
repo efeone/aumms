07eddf3c968965fafedb90ab7cb3572f74c60d37
Muhammed Sinan K T <muhammed786sinu000@gmail.com>
2024-03-16 14:44:51 +0530

    feat: Added board amount calculation based on board rate

--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
@@ -2,33 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Customer Jewellery Order", {
-	refresh(frm) {
-		if (!frm.is_new()){
-			frm.add_custom_button('Jewellery Order', () => {
-				frappe.call('aumms.aumms_manufacturing.doctype.customer_jewellery_order.customer_jewellery_order.create_jewellery_order', {
-					customer_jewellery_order : frm.doc.name
-				}).then(r =>{
-						frm.reload_doc();
-				});
-			},'Create');
-		}
-	},
+  customer_expected_total_weight: function (frm) {
+    calculate_customer_expected_amount(frm);
+  },
+  uom: function (frm) {
+    calculate_customer_expected_amount(frm);
+  },
+  purity: function (frm) {
+    calculate_customer_expected_amount(frm);
+  },
 });
 
-frappe.ui.form.on("Customer Jewellery Order Details",{
-  expected_weight_per_quantity: function(frm, cdt, cdn){
-   let d = locals[cdt][cdn];
-   var total_weightage = 0
-   frm.doc.order_item.forEach(function(d){
-     total_weightage += d.expected_weight_per_quantity * d.item_quantity;
-   })
-   frm.set_value('total_expected_weight_per_quantity',total_weightage)
- },
- order_item_remove:function(frm){
-     var total_weightage = 0
-     frm.doc.order_item.forEach(function(d){
-       total_weightage += d.expected_weight_per_quantity;
-     })
-     frm.set_value("total_expected_weight_per_quantity",total_weightage)
-   }
-})
+frappe.ui.form.on("Customer Jewellery Order Details", {
+  expected_weight_per_quantity: function (frm, cdt, cdn) {
+    let d = locals[cdt][cdn];
+    var total_weightage = 0;
+    frm.doc.order_item.forEach(function (d) {
+      total_weightage += d.expected_weight_per_quantity * d.item_quantity;
+    });
+    frm.set_value("total_expected_weight_per_quantity", total_weightage);
+  },
+
+  order_item_remove: function (frm) {
+    var total_weightage = 0;
+    frm.doc.order_item.forEach(function (d) {
+      total_weightage += d.expected_weight_per_quantity;
+    });
+    frm.set_value("total_expected_weight_per_quantity", total_weightage);
+  },
+});
+
+function calculate_customer_expected_amount(frm) {
+  if (frm.doc.customer_expected_total_weight && frm.doc.uom && frm.doc.purity) {
+    board_rate = frm.call("calculate_customer_expected_amount");
+  }
+}
--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
@@ -5,32 +5,66 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 
+
 class CustomerJewelleryOrder(Document):
-	def validate(self):
-		if self.total_expected_weight_per_quantity != self.customer_expected_total_weight:
-			frappe.throw(_('The Sum of Expected Weights Per Quantity must be Equal to Customer Expected Weight'))
+    def validate(self):
+        if (
+            self.total_expected_weight_per_quantity
+            != self.customer_expected_total_weight
+        ):
+            frappe.throw(
+                _(
+                    "The Sum of Expected Weights Per Quantity must be Equal to Customer Expected Weight"
+                )
+            )
 
+    def on_submit(self):
+        self.create_jewellery_order()
 
-@frappe.whitelist()
-def create_jewellery_order(customer_jewellery_order):
-    if frappe.db.exists('Customer Jewellery Order', customer_jewellery_order):
-        doc = frappe.get_doc('Customer Jewellery Order', customer_jewellery_order)
-        if not frappe.db.exists('Jewellery Order', {'customer_jewellery_order': doc.name}):
-            for item in doc.order_item:
-                jewellery_order = frappe.new_doc('Jewellery Order')
-                jewellery_order.customer_jewellery_order = doc.name
-                jewellery_order.customer = doc.customer
-                jewellery_order.required_date = doc.required_date
-                jewellery_order.customer_expected_total_weight = doc.customer_expected_total_weight
-                jewellery_order.customer_expected_amount = doc.customer_expected_amount
-                jewellery_order.item_category = item.item_category
-                jewellery_order.item_type = item.item_type
-                jewellery_order.quantity = item.item_quantity
-                jewellery_order.expected_weight_per_quantity = item.expected_weight_per_quantity
-                jewellery_order.design_attachment = item.item_design_attachment
-                jewellery_order.insert(ignore_permissions=True)
-                frappe.msgprint('Jewellery Order Created.', indicator="green", alert=1)
+    def create_jewellery_order(self):
+        jewellery_order_exist = frappe.db.exists(
+            "Jewellery Order", {"customer_jewellery_order": self.name}
+        )
+        if not jewellery_order_exist:
+            for item in self.order_item:
+                new_jewellery_order = frappe.new_doc("Jewellery Order")
+                new_jewellery_order.customer_jewellery_order = self.name
+                new_jewellery_order.customer = self.customer
+                new_jewellery_order.required_date = self.required_date
+                new_jewellery_order.customer_expected_total_weight = (
+                    self.customer_expected_total_weight
+                )
+                new_jewellery_order.customer_expected_amount = (
+                    self.customer_expected_amount
+                )
+                new_jewellery_order.item_category = item.item_category
+                new_jewellery_order.item_type = item.item_type
+                new_jewellery_order.quantity = item.item_quantity
+                new_jewellery_order.expected_weight_per_quantity = (
+                    item.expected_weight_per_quantity
+                )
+                new_jewellery_order.design_attachment = item.item_design_attachment
+                new_jewellery_order.insert(ignore_permissions=True)
+                frappe.msgprint(
+                    f"Jewellery Order {new_jewellery_order.name} Created.",
+                    indicator="green",
+                    alert=1,
+                )
         else:
-            frappe.throw(_('Jewellery Order is already exist for {0}'.format(doc.name)))
-    else:
-        frappe.throw(_('Customer Jewellery Order {0} does not exist'.format(customer_jewellery_order)))
+            frappe.throw(_(f"Jewellery Order is already exist for {self.name}"))
+
+    @frappe.whitelist()
+    def calculate_customer_expected_amount(self):
+        exists = frappe.db.exists("Board Rate", {"purity": self.purity, "uom": "Gram"})
+        if exists:
+            latest_board_rate = frappe.get_last_doc(
+                "Board Rate", {"purity": self.purity, "uom": "Gram"}
+            )
+            board_rate_value = latest_board_rate.board_rate
+            self.customer_expected_amount = (
+                self.customer_expected_total_weight * board_rate_value
+            )
+        else:
+            frappe.throw(
+                f"No Board Rate found for Purity {self.purity} and UOM {self.uom}"
+            )
