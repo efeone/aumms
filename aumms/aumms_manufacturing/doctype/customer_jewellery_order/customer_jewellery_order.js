b696bfdd51f428e9be65496e904325fee92a9a3a
Geethu K <geethuk626@gmail.com>
2024-03-21 09:57:35 +0530

    feat: Set Validation, Filters and Other changes in Raw Material Required Doctype

--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
@@ -31,7 +31,14 @@ frappe.ui.form.on("Customer Jewellery Order Details", {
     });
     frm.set_value("total_expected_weight_per_quantity", total_weightage);
   },
-
+  item_quantity: function (frm, cdt, cdn) {
+    let d = locals[cdt][cdn];
+    var total_weightage = 0;
+    frm.doc.order_item.forEach(function (d) {
+      total_weightage += d.expected_weight_per_quantity * d.item_quantity;
+    });
+    frm.set_value("total_expected_weight_per_quantity", total_weightage);
+  },
   order_item_remove: function (frm) {
     var total_weightage = 0;
     frm.doc.order_item.forEach(function (d) {
