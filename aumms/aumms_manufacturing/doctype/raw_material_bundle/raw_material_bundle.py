0e65458bc588669ab67b8db835f0e2c68473b144
Keerthana-ck <keerthana98323@gmail.com>
2024-03-25 11:35:00 +0530

    fix : Fixed Minor Issues

--- aumms/aumms_manufacturing/doctype/raw_material_bundle/raw_material_bundle.py
+++ aumms/aumms_manufacturing/doctype/raw_material_bundle/raw_material_bundle.py
@@ -16,14 +16,16 @@ class RawMaterialBundle(Document):
 		self.create_raw_materiel_request()
 
 	def create_raw_materiel_request(self):
-		raw_materiel_request_exists = frappe.db.exists('Raw Material Request',{'manufacturing_request': self.manufacturing_request})
-		if not raw_materiel_request_exists:
-			raw_materiel_count = 0
-			for raw_material in self.raw_material_details :
-				if raw_material.quantity > raw_material.available_quantity_in_stock:
+		raw_materiel_count = 0
+		for raw_material in self.raw_material_details:
+			if raw_material.quantity > raw_material.available_quantity_in_stock:
+				raw_materiel_request_exists = frappe.db.exists('Raw Material Request', {
+				'manufacturing_request': self.manufacturing_request,
+				'item_name': raw_material.item_name
+	            })
+				if not raw_materiel_request_exists:
 					new_raw_materiel_request = frappe.new_doc('Raw Material Request')
 					new_raw_materiel_request.raw_material_request_type = "Manufacturing Request"
-					new_raw_materiel_request.raw_materiel_bundle = self.name
 					new_raw_materiel_request.manufacturing_request = self.manufacturing_request
 					new_raw_materiel_request.required_date = self.item_required_date
 					new_raw_materiel_request.required_quantity = raw_material.quantity - raw_material.available_quantity_in_stock
@@ -32,6 +34,4 @@ class RawMaterialBundle(Document):
 					new_raw_materiel_request.raw_material_bundle = raw_material.raw_material_id
 					new_raw_materiel_request.insert(ignore_permissions=True)
 					raw_materiel_count += 1
-			frappe.msgprint(f"{raw_materiel_count} Raw Material Request Created.",indicator="green",alert=1,)
-		else:
-			frappe.throw(_(f"Raw Material Request is already exist for {self.name}"))
+		frappe.msgprint(f"{raw_materiel_count} Raw Material Requests Created.", indicator="green", alert=1)
