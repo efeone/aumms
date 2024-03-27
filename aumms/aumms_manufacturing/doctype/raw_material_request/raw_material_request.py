a00fdd02518aa1a62c657af8002b928ad6a82d2c
Geethu K <geethuk626@gmail.com>
2024-03-27 22:14:49 +0530

    feat:Miner Issue Fixing

--- aumms/aumms_manufacturing/doctype/raw_material_request/raw_material_request.py
+++ aumms/aumms_manufacturing/doctype/raw_material_request/raw_material_request.py
@@ -8,22 +8,20 @@ from frappe.model.document import Document
 
 class RawMaterialRequest(Document):
 
-    def on_submit(self):
-        if self.not_available == 1:
-            if self.raw_material_request_type == 'Manufacturing Request':
-                self.create_manufacturing_request()
-                self.create_purchase_order()
-
     @frappe.whitelist()
     def create_manufacturing_request(self):
         manufacturing_request_exists = frappe.db.exists('Manufacturing Request', {'raw_material_request': self.name})
         if not manufacturing_request_exists:
             manufacturing_request = frappe.new_doc('Manufacturing Request')
-            manufacturing_request.raw_material_request_type = self.raw_material_request_type
+            manufacturing_request.raw_material_request_type = 'Raw Material Request'
             manufacturing_request.raw_material_request = self.name
             manufacturing_request.jewellery_order = self.jewellery_order
             manufacturing_request.required_date = self.required_date
-            manufacturing_request.weight = self.weight
+            manufacturing_request.quantity = self.required_quantity
+            manufacturing_request.total_weight = self.weight
+            manufacturing_request.item_name = self.item_name
+            manufacturing_request.uom = self.uom
+            manufacturing_request.type = self.item_type
             manufacturing_request.insert(ignore_permissions=True)
             frappe.msgprint(f"Manufacturing Request {manufacturing_request.name} created.", indicator="green", alert=1)
         else:
