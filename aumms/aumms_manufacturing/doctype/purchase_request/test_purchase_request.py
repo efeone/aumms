4d7212c61bdba6ac2ac7de864cb9dda83f3d77d4
Keerthana-ck <keerthana98323@gmail.com>
2024-03-28 11:02:04 +0530

    fix : Makes some Fields as Mandatory

--- aumms/aumms_manufacturing/doctype/purchase_request/test_purchase_request.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, efeone and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPurchaseRequest(FrappeTestCase):
-	pass
