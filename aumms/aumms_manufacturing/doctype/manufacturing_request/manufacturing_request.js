c9c9cc13c6f023838fbc77927caba7b42fff3d5c
Geethu K <geethuk626@gmail.com>
2024-03-28 16:52:29 +0530

    feat:Update Status in Manufacturing Request

--- aumms/aumms_manufacturing/doctype/manufacturing_request/manufacturing_request.js
+++ aumms/aumms_manufacturing/doctype/manufacturing_request/manufacturing_request.js
@@ -10,28 +10,40 @@ frappe.ui.form.on("Manufacturing Request", {
 				}
 			}
 		});
+  }
+});
+
+frappe.ui.form.on("Manufacturing Request Stage", {
+  select_raw_material: function(frm, cdt , cdn) {
+    let row = locals[cdt][cdn]
+    frappe.new_doc('Raw Material Bundle', {
+      'manufacturing_request': frm.doc.name,
+      'manufacturing_stage' : row.manufacturing_stage,
+    })
   },
-  });
-  frappe.ui.form.on("Manufacturing Request Stage", {
-    select_raw_material: function(frm, cdt , cdn) {
-      let row = locals[cdt][cdn]
-      frappe.new_doc('Raw Material Bundle', {
-        'manufacturing_request': frm.doc.name,
-        'manufacturing_stage' : row.manufacturing_stage,
+  create_job_card: function(frm, cdt, cdn) {
+    frm.call('create_jewellery_job_card', { 'stage_row_id': cdn }).then(r => {
+        frm.refresh_fields();
+    });
+  },
+  awaiting_raw_material: function(frm, cdt, cdn) {
+    let row = locals[cdt][cdn]
+    if (row.awaiting_raw_material) {
+      frm.call('update_previous_stage', {idx:row.idx}).then(r=>{
+        row.previous_stage = r.message
+        frm.refresh_fields()
       })
-    },
-    create_job_card: function(frm, cdt, cdn) {
-      frm.call('create_jewellery_job_card', { 'stage_row_id': cdn }).then(r => {
-          frm.refresh_fields();
-      });
-    },
-    awaiting_raw_material: function(frm, cdt, cdn) {
-      let row = locals[cdt][cdn]
-      if (row.awaiting_raw_material) {
-        frm.call('update_previous_stage', {idx:row.idx}).then(r=>{
-          row.previous_stage = r.message
-          frm.refresh_fields()
-        })
-      }
+    }
+  },
+  completed: function(frm, cdt, cdn) {
+        let allcompleted = true;
+        let childTable = frm.doc.manufacturing_request_stage;
+        for (let i = 0; i < childTable.length; i++) {
+            if (!childTable[i].completed) {
+                allcompleted = false;
+                break;
+            }
+        }
+        frm.set_value('status', allcompleted ? 'Completed' : 'Open');
     }
 });
